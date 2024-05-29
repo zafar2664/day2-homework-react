ae01af17690d6ed986e2268ecc8a344da3b9691b
Zafar Alam <sheikhzafar2664@gmail.com>
2024-05-29 22:54:13 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='main'>
+      <div>
+        <h1>Topics Covered</h1>
+      </div>
+      <div>
+        <p>The following is  list of all the topics we cover in the MDN learning area</p>
+      </div>
+      <div>
+        <h2>Getting started with the web</h2>
+      </div>
+      <div>
+        <p>provides a practical introduction to web development for complete beginners</p>
+      </div>
+
+      <div>
+        <h2>HTML- Structuring the web</h2>
+        <p>Html is the language that we use to structure the different parts of our content</p>
+        <p>and define what their meaning or purpose is. This topic teaches HTML in detail.</p>
+      </div>
+
+      <div>
+        <h2>CSS-Styling the web</h2>
+        <p>CSS is the language that we use to control our web contents style and layout, as</p>
+        <p>well as adding behaviour like animation. This topic provides comprehensive</p>
+        <p>coverege of CSS</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
